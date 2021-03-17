1a85d72a1bfa963f944504eb211b8d51e6c00611
liujia <https://exmail.qq.com/login>
2021-03-17 17:25:58 +0800

    路由改hash

--- src/components/common/form/ComText.vue
+++ src/components/common/form/ComText.vue
@@ -1,25 +1,50 @@
 <template>
-	<el-input :value="value" type="text" placeholder="请输入内容" @input="$emit('input', $event)" />
+	<el-input :value="value" type="text" :placeholder="placeholder" :minlength="minlength" :maxlength="maxlength"
+		:show-word-limit="showWordLimit" :clearable="clearable" @input="$emit('input', $event)">
+		<template #suffix>
+			<slot name="suffix"></slot>
+		</template>
+		<template #prefix>
+			<slot name="prefix"></slot>
+		</template>
+	</el-input>
 </template>
 
 <script>
-export default {
-	name: 'ComText',
-	props: {
-		value: {
-      type: String,
-      required: true
-    }
-	},
-	data() {
-		return {
-			icontest: 'iconcloud'
-		};
-	},
-	methods:{
-		
-	}
-};
+	export default {
+		name: 'ComText',
+		props: {
+			value: {
+				type: String,
+				required: true
+			},
+			placeholder: {
+				type: String,
+			},
+			minlength: {
+				type: Number,
+			},
+			maxlength: {
+				type: Number,
+			},
+			showWordLimit: {
+				type: Boolean,
+				default: false
+			},
+			clearable: {
+				type: Boolean,
+				default: true
+			},
+		},
+		data() {
+			return {
+				icontest: 'iconcloud'
+			};
+		},
+		methods: {
+
+		}
+	};
 </script>
 
 <style></style>
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,42 +1,55 @@
 <template>
 	<div class="main">
-		<div class="login"><com-text v-model="username" />
-		<com-form value="777"/>
+		<div class="login">
+			<el-input v-model="username" type="text" :minlength="5" :maxlength="10" :show-word-limit="true" placeholder="请输入内容" />
+			<com-text v-model="username" placeholder="啥" :maxlength="20" :show-word-limit="true">
+				<template #prefix>
+					<i class="iconfont icondiff"></i>
+				</template>
+			</com-text>
+			<com-form value="777" />
 		</div>
 	</div>
 </template>
 
 <script>
-import ComText from '@/components/common/form/ComText.vue';
-import ComForm from '@/components/common/form/ComForm.vue';
-export default {
-	name: 'Login',
-	components: {
-		ComText,
-		ComForm
-	},
-	data() {
-		return {
-			username: 'ccc'
-		};
-	}
-};
+	import ComText from '@/components/common/form/ComText.vue';
+	import ComForm from '@/components/common/form/ComForm.vue';
+	export default {
+		name: 'Login',
+		components: {
+			ComText,
+			ComForm
+		},
+		data() {
+			return {
+				username: 'ccc'
+			};
+		}
+	};
 </script>
 
 <style scoped lang="scss">
-.main {
-	width: 100vw;
-	height: 100vh;
-	background-image: url(~@/assets/img/bg_login.jpg);
-	background-position: center;
-	background-repeat: no-repeat;
-	background-size: cover;
-	.login {
-		position: fixed;
-		top: 50%;
-		padding: 20px;
-		border-radius: 10px;
-		background-color: var(--color-bg-default);
+	.main {
+		width: 100vw;
+		height: 100vh;
+		background-image: url(~@/assets/img/bg_login.jpg);
+		background-position: center;
+		background-repeat: no-repeat;
+		background-size: cover;
+
+		.login {
+			position: fixed;
+			top: 50%;
+			padding: 20px;
+			border-radius: 10px;
+			background-color: var(--color-bg-default);
+			
+			.iconfont {
+				display: inline-block;
+				width: 25px;
+				line-height: 40px;
+			}
+		}
 	}
-}
 </style>
